51400484b45e2e2efb819f4d659f0be073b271f8
Scott R Charlton <charlton@usgs.gov>
2009-03-03 08:19:06 +0000

    sync'd with src/basic.c revision 3196
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqci/trunk@3401 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- phreeqc/extra/basic1.c
+++ phreeqc/extra/basic1.c
@@ -2,7 +2,7 @@
 /* From input file "basic.p" */
 
 
-static char const svnid[] = "$Id: basic.c 3188 2008-11-14 00:06:34Z dlpark $";
+static char const svnid[] = "$Id: basic.c 3196 2008-11-17 16:16:59Z dlpark $";
 
 #define EXTERNAL extern
 #include "../src/global.h"
@@ -30,131 +30,131 @@ extern int colnr, rownr;
 
 static int sget_logical_line(char **ptr, int *l, char *return_line);
 
-#define checking        true
+#define checking	true
 #define varnamelen      20
-#define maxdims         4
+#define maxdims	 4
 
 typedef Char varnamestring[varnamelen + 1];
 typedef Char string255[256];
 
-#define tokvar          0
-#define toknum          1
-#define tokstr          2
-#define toksnerr        3
-#define tokplus         4
-#define tokminus        5
-#define toktimes        6
-#define tokdiv          7
-#define tokup           8
-#define toklp           9
-#define tokrp           10
-#define tokcomma        11
-#define toksemi         12
-#define tokcolon        13
-#define tokeq           14
-#define toklt           15
-#define tokgt           16
-#define tokle           17
-#define tokge           18
-#define tokne           19
-#define tokand          20
-#define tokor           21
-#define tokxor          22
-#define tokmod          23
-#define toknot          24
-#define toksqr          25
-#define toksqrt         26
-#define toksin          27
-#define tokcos          28
-#define toktan          29
+#define tokvar	  0
+#define toknum	  1
+#define tokstr	  2
+#define toksnerr	3
+#define tokplus	 4
+#define tokminus	5
+#define toktimes	6
+#define tokdiv	  7
+#define tokup	   8
+#define toklp	   9
+#define tokrp	   10
+#define tokcomma	11
+#define toksemi	 12
+#define tokcolon	13
+#define tokeq	   14
+#define toklt	   15
+#define tokgt	   16
+#define tokle	   17
+#define tokge	   18
+#define tokne	   19
+#define tokand	  20
+#define tokor	   21
+#define tokxor	  22
+#define tokmod	  23
+#define toknot	  24
+#define toksqr	  25
+#define toksqrt	 26
+#define toksin	  27
+#define tokcos	  28
+#define toktan	  29
 #define tokarctan       30
-#define toklog          31
-#define tokexp          32
-#define tokabs          33
-#define toksgn          34
-#define tokstr_         35
-#define tokval          36
-#define tokchr_         37
-#define tokasc          38
-#define toklen          39
-#define tokmid_         40
-#define tokpeek         41
-#define tokrem          42
-#define toklet          43
-#define tokprint        44
-#define tokinput        45
-#define tokgoto         46
-#define tokif           47
-#define tokend          48
-#define tokstop         49
-#define tokfor          50
-#define toknext         51
-#define tokwhile        52
-#define tokwend         53
-#define tokgosub        54
+#define toklog	  31
+#define tokexp	  32
+#define tokabs	  33
+#define toksgn	  34
+#define tokstr_	 35
+#define tokval	  36
+#define tokchr_	 37
+#define tokasc	  38
+#define toklen	  39
+#define tokmid_	 40
+#define tokpeek	 41
+#define tokrem	  42
+#define toklet	  43
+#define tokprint	44
+#define tokinput	45
+#define tokgoto	 46
+#define tokif	   47
+#define tokend	  48
+#define tokstop	 49
+#define tokfor	  50
+#define toknext	 51
+#define tokwhile	52
+#define tokwend	 53
+#define tokgosub	54
 #define tokreturn       55
-#define tokread         56
-#define tokdata         57
+#define tokread	 56
+#define tokdata	 57
 #define tokrestore      58
 #define tokgotoxy       59
-#define tokon           60
-#define tokdim          61
-#define tokpoke         62
-#define toklist         63
-#define tokrun          64
-#define toknew          65
-#define tokload         66
-#define tokmerge        67
-#define toksave         68
-#define tokbye          69
-#define tokdel          70
-#define tokrenum        71
-#define tokthen         72
-#define tokelse         73
-#define tokto           74
-#define tokstep         75
-#define toktc           76
-#define tokm0           77
-#define tokm            78
-#define tokparm         79
-#define tokact          80
-#define tokmol          81
-#define tokla           82
-#define toklm           83
-#define toksr           84
-#define toksi           85
-#define toktot          86
-#define toktk           87
-#define toktime         88
-#define toklog10        89
+#define tokon	   60
+#define tokdim	  61
+#define tokpoke	 62
+#define toklist	 63
+#define tokrun	  64
+#define toknew	  65
+#define tokload	 66
+#define tokmerge	67
+#define toksave	 68
+#define tokbye	  69
+#define tokdel	  70
+#define tokrenum	71
+#define tokthen	 72
+#define tokelse	 73
+#define tokto	   74
+#define tokstep	 75
+#define toktc	   76
+#define tokm0	   77
+#define tokm	    78
+#define tokparm	 79
+#define tokact	  80
+#define tokmol	  81
+#define tokla	   82
+#define toklm	   83
+#define toksr	   84
+#define toksi	   85
+#define toktot	  86
+#define toktk	   87
+#define toktime	 88
+#define toklog10	89
 #define toksim_time     90
-#define tokequi         91
-#define tokgas          92
-#define tokpunch        93
-#define tokkin          94
-#define toks_s          95
-#define tokmu           96
-#define tokalk          97
-#define tokrxn          98
-#define tokdist         99
-#define tokmisc1        100
-#define tokmisc2        101
-#define tokedl          102
+#define tokequi	 91
+#define tokgas	  92
+#define tokpunch	93
+#define tokkin	  94
+#define toks_s	  95
+#define tokmu	   96
+#define tokalk	  97
+#define tokrxn	  98
+#define tokdist	 99
+#define tokmisc1	100
+#define tokmisc2	101
+#define tokedl	  102
 #define tokstep_no      103
 #define toksim_no       104
 #define toktotal_time   105
-#define tokput          106
-#define tokget          107
+#define tokput	  106
+#define tokget	  107
 #define tokcharge_balance  109
 #define tokpercent_error   110
 #ifdef PHREEQ98
-#define tokgraph_x        111
-#define tokgraph_y        112
+#define tokgraph_x	111
+#define tokgraph_y	112
 #define tokgraph_sy       113
 #endif
 #define tokcell_no      114
 #define tokexists       115
-#define toksurf         116
+#define toksurf	 116
 #define toklk_species   117
 #define toklk_named     118
 #define toklk_phase     119
@@ -163,28 +163,28 @@ typedef Char string255[256];
 #define toksum_s_s      122
 #define tokcalc_value   123
 #define tokdescription  124
-#define toksys          125
-#define tokinstr        126
-#define tokltrim        127
-#define tokrtrim        128
-#define toktrim         129
-#define tokpad          130
+#define toksys	  125
+#define tokinstr	126
+#define tokltrim	127
+#define tokrtrim	128
+#define toktrim	 129
+#define tokpad	  130
 #define tokchange_por   131
-#define tokget_por    	132
-#define tokosmotic    	133
+#define tokget_por	    132
+#define tokosmotic	    133
 #define tokchange_surf  134
 #define tokporevolume   135
-#define toksc        136
-#define tokgamma        137
-#define toklg           138
-// VP: Density Start
-#define tokrho           139
-// VP: Density End
+#define toksc	136
+#define tokgamma	137
+#define toklg	   138
+/* VP: Density Start */
+#define tokrho	   139
+/* VP: Density End */
 
 typedef LDBLE numarray[];
 typedef Char *strarray[];
 
-#define forloop         0
+#define forloop	 0
 #define whileloop       1
 #define gosubloop       2
 
@@ -418,11 +418,10 @@ static const struct const_key command[] = {
 	{"porevolume", tokporevolume},
 	{"sc", toksc},
 	{"gamma", tokgamma},
-// VP: Density Start
-//	{"lg", toklg}
+/* VP: Density Start */
 	{"lg", toklg},
 	{"rho", tokrho}
-// VP: Density End
+/* VP: Density End */
 };
 static int NCMDS = (sizeof(command) / sizeof(struct const_key));
 
@@ -824,9 +823,9 @@ sget_logical_line(char **ptr, int *l, char *return_line)
  *   reallocs line_save and line if more space is needed
  *
  *   returns:
- *           EOF on empty line on end of file or
- *           OK otherwise
- *           *l returns length of line
+ *	   EOF on empty line on end of file or
+ *	   OK otherwise
+ *	   *l returns length of line
  */
 	int i;
 	char c;
@@ -2114,11 +2113,11 @@ listtokens(FILE * f, tokenrec * buf)
 			output_msg(OUTPUT_BASIC, "LG");
 			break;
 
-// VP: Density Start
+/* VP: Density Start */
 		case tokrho:
 			output_msg(OUTPUT_BASIC, "RHO");
 			break;
-// VP: Density End
+/* VP: Density End */
 		}
 		buf = buf->next;
 	}
@@ -3414,11 +3413,11 @@ factor(struct LOC_exec * LINK)
 		n.UU.val = pore_volume;
 		break;
 
-// VP : Density Start
+/* VP : Density Start */
 	case tokrho:
 		n.UU.val = calc_dens();
 		break;
-// VP: Density End
+/* VP: Density End */
 
 	case toksc:
 		n.UU.val = calc_SC();
@@ -3972,7 +3971,7 @@ cmdlist(struct LOC_exec *LINK)
 			if (l->num >= n1)
 			{
 				/* printf("%ld ", l->num); */
-				/*        listtokens(stdout, l->txt); */
+				/*	listtokens(stdout, l->txt); */
 				/* putchar('\n'); */
 				output_msg(OUTPUT_MESSAGE, "%ld ", l->num);
 				listtokens(NULL, l->txt);
@@ -4214,7 +4213,7 @@ cmdchange_surf(struct LOC_exec *LINK)
 {
 /*
     change_surf("Hfo",    0.3,      "Sfo",      0,      5      )
-               (old_name, fraction, new_name, new_Dw, cell_no)
+	       (old_name, fraction, new_name, new_Dw, cell_no)
  */
 	char *c1;
 	int count;
@@ -5679,3 +5678,4 @@ free_dim_stringvar(struct varrec *varbase)
 	}
 	return (OK);
 }
+
