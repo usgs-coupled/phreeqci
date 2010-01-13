0f07c0c3b474ea174e25282b91a3b7c29199d67f
Scott R Charlton <charlton@usgs.gov>
2010-01-13 02:14:14 +0000

    Added new classes for SURFACE keyword
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqci/branches/VS2005@3916 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ SurfaceSheet.cpp
@@ -0,0 +1,395 @@
+// SurfaceSheet.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "phreeqci2.h"
+#include "SurfaceSheet.h"
+
+#include "KeywordLoader2.h"
+
+// CSurfaceSheet
+
+IMPLEMENT_DYNAMIC(CSurfaceSheet, baseCSurfaceSheet)
+
+// COMMENT: {12/22/2009 6:18:52 PM}CSurfaceSheet::CSurfaceSheet(UINT nIDCaption, CWnd* pParentWnd, UINT iSelectPage)
+// COMMENT: {12/22/2009 6:18:52 PM}	:baseCSurfaceSheet(nIDCaption, pParentWnd, iSelectPage)
+// COMMENT: {12/22/2009 6:18:52 PM}{
+// COMMENT: {12/22/2009 6:18:52 PM}	AddPage(&this->Page);
+// COMMENT: {12/22/2009 6:18:52 PM}}
+// COMMENT: {12/22/2009 6:18:52 PM}
+// COMMENT: {12/22/2009 6:18:52 PM}CSurfaceSheet::CSurfaceSheet(LPCTSTR pszCaption, CWnd* pParentWnd, UINT iSelectPage)
+// COMMENT: {12/22/2009 6:18:52 PM}	:baseCSurfaceSheet(pszCaption, pParentWnd, iSelectPage)
+// COMMENT: {12/22/2009 6:18:52 PM}{
+// COMMENT: {12/22/2009 6:18:52 PM}	AddPage(&this->Page);
+// COMMENT: {12/22/2009 6:18:52 PM}}
+
+CSurfaceSheet::CSurfaceSheet(CWnd* pWndParent, CTreeCtrlNode simNode)
+: baseCSurfaceSheet(IDS_PROPSHT_CAPTION18, pWndParent)
+, DefinedRanges(simNode, true)
+// COMMENT: {12/28/2009 9:23:01 PM}, EquilSolutionNum(CSurfaceSheet::N_NONE)
+{
+	this->SolutionNumberFormat = CUtil::CreateRange(this->SetSolutions, this->DefinedRanges[CKeyword::K_SOLUTION]);
+	AddPage(&this->Page);
+}
+
+CSurfaceSheet::~CSurfaceSheet()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CSurfaceSheet, baseCSurfaceSheet)
+END_MESSAGE_MAP()
+
+
+// CSurfaceSheet message handlers
+CString CSurfaceSheet::GetString()
+{
+	CString strLines;
+	
+	// Line 0
+	strLines = GetLineZero(CKeyword::K_SURFACE);
+
+	CString strFormat;
+
+	// Line 1
+	if (this->Page.n_solution != CSurfacePage::N_NONE)
+	{
+		strFormat.Format(_T("%s%4c-equilibrate with solution %d"),
+			(LPCTSTR)s_strNewLine,
+			_T(' '),
+			this->Page.n_solution
+			);
+		strLines += strFormat;
+	}
+
+	// sites units
+	switch (this->Page.sites_units)
+	{
+	case SITES_DENSITY:
+		strFormat.Format(_T("%s%4c-sites DENSITY"),
+			(LPCTSTR)s_strNewLine,
+			_T(' ')
+			);
+		strLines += strFormat;
+		break;
+
+	case SITES_ABSOLUTE:
+		break;
+
+	default:
+		ASSERT(FALSE);
+		break;
+	}
+
+	// Line 2
+	std::list<CSurfComp>::const_iterator const_iter = this->Page.ListSurfComp[CSurfacePage::TI_GENERAL].begin();
+	std::set<CString> setSurfaces;
+	for ( ; const_iter != this->Page.ListSurfComp[CSurfacePage::TI_GENERAL].end(); ++const_iter)
+	{
+		CString strSurface = (*const_iter).m_strFormula.SpanExcluding(_T("_"));
+		std::pair<std::set<CString>::iterator, bool> ans = setSurfaces.insert(strSurface);
+		if (ans.second)
+		{
+			strFormat.Format(_T("%s%4c%-7s    %-6lg    %-6lg    %-6lg"),
+				(LPCTSTR)s_strNewLine,
+				_T(' '),
+				(*const_iter).m_strFormula,
+				(*const_iter).m_dMoles,
+				(*const_iter).m_dSpecific_area,
+				(*const_iter).m_dGrams
+				);
+			if (this->Page.transport)
+			{
+				CString extra;
+				if ((*const_iter).m_dDw > 0)
+				{
+					extra.Format(_T("%8cDw %-6lg"),
+						_T(' '),
+						(*const_iter).m_dDw
+						);
+				}
+				strFormat += extra;
+			}
+			strFormat.TrimRight();
+			strLines += strFormat;
+
+			if (this->Page.type == CD_MUSIC)
+			{
+				strFormat.Format(_T("%s%8c-capacitance %-6lg %-6lg"),
+					(LPCTSTR)s_strNewLine,
+					_T(' '),
+					(*const_iter).m_dCapacitance0,
+					(*const_iter).m_dCapacitance1
+					);
+				strFormat.TrimRight();
+				strLines += strFormat;
+			}
+		}
+		else
+		{
+			strFormat.Format(_T("%s%4c%-7s    %-6lg"),
+				(LPCTSTR)s_strNewLine,
+				_T(' '),
+				(*const_iter).m_strFormula,
+				(*const_iter).m_dMoles
+				);
+			strFormat.TrimRight();
+			strLines += strFormat;
+		}
+	}
+
+	// Line 3
+	const_iter = this->Page.ListSurfComp[CSurfacePage::TI_EQUILIBRIUM_PHASES].begin();
+	setSurfaces.clear();
+	for ( ; const_iter != this->Page.ListSurfComp[CSurfacePage::TI_EQUILIBRIUM_PHASES].end(); ++const_iter)
+	{
+		CString strSurface = (*const_iter).m_strFormula.SpanExcluding(_T("_"));
+		std::pair<std::set<CString>::iterator, bool> ans = setSurfaces.insert(strSurface);
+		if (ans.second)
+		{
+			strFormat.Format(_T("%s%4c%-7s %-15s equilibrium_phase %-6lg %-6lg"),
+				(LPCTSTR)s_strNewLine,
+				_T(' '),
+				(*const_iter).m_strFormula,
+				(*const_iter).m_strPhase_name,
+				(*const_iter).m_dPhase_proportion,
+				(*const_iter).m_dSpecific_area
+				);
+		}
+		else
+		{
+			strFormat.Format(_T("%s%4c%-7s %-15s equilibrium_phase %-6lg"),
+				(LPCTSTR)s_strNewLine,
+				_T(' '),
+				(*const_iter).m_strFormula,
+				(*const_iter).m_strPhase_name,
+				(*const_iter).m_dPhase_proportion
+				);
+		}
+		strFormat.TrimRight();
+		strLines += strFormat;
+	}
+
+	// Line 3
+	const_iter = this->Page.ListSurfComp[CSurfacePage::TI_KINETIC_REACTANTS].begin();
+	setSurfaces.clear();
+	for ( ; const_iter != this->Page.ListSurfComp[CSurfacePage::TI_KINETIC_REACTANTS].end(); ++const_iter)
+	{
+		CString strSurface = (*const_iter).m_strFormula.SpanExcluding(_T("_"));
+		std::pair<std::set<CString>::iterator, bool> ans = setSurfaces.insert(strSurface);
+		if (ans.second)
+		{
+			strFormat.Format(_T("%s%4c%-7s %-15s kinetic           %-6lg %-6lg"),
+				(LPCTSTR)s_strNewLine,
+				_T(' '),
+				(*const_iter).m_strFormula,
+				(*const_iter).m_strRate_name,
+				(*const_iter).m_dPhase_proportion,
+				(*const_iter).m_dSpecific_area
+				);
+		}
+		else
+		{
+			strFormat.Format(_T("%s%4c%-7s %-15s kinetic           %-6lg"),
+				(LPCTSTR)s_strNewLine,
+				_T(' '),
+				(*const_iter).m_strFormula,
+				(*const_iter).m_strRate_name,
+				(*const_iter).m_dPhase_proportion
+				);
+		}
+		strFormat.TrimRight();
+		strLines += strFormat;
+	}
+
+	
+	switch (this->Page.type)
+	{
+	case NO_EDL:
+		strFormat.Format(_T("%s%4c-no_edl"),
+			(LPCTSTR)s_strNewLine,
+			_T(' ')
+			);
+		strLines += strFormat;
+		break;
+
+	case DDL:
+		// do nothing
+		break;
+
+	case CD_MUSIC:
+		strFormat.Format(_T("%s%4c-cd_music"),
+			(LPCTSTR)s_strNewLine,
+			_T(' ')
+			);
+		strLines += strFormat;
+		break;
+	}
+
+	// Lines 4, 5, and 6
+	switch (this->Page.dl_type)
+	{
+	case NO_DL:
+		break;
+
+	case DONNAN_DL:
+		if (this->Page.DT == CSurfacePage::DT_DEBYE_LENGTHS)
+		{
+			if (this->Page.DDL_viscosity != 1.)
+			{
+				strFormat.Format(_T("%s%4c-donnan debye_lengths %.*g limit_ddl %.*g viscosity %.*g"),
+					(LPCTSTR)s_strNewLine,
+					_T(' '),
+					DBL_DIG,
+					this->Page.debye_lengths,
+					DBL_DIG,
+					this->Page.DDL_limit,
+					DBL_DIG,
+					this->Page.DDL_viscosity
+					);
+			}
+			else
+			{
+				strFormat.Format(_T("%s%4c-donnan debye_lengths %.*g limit_ddl %.*g"),
+					(LPCTSTR)s_strNewLine,
+					_T(' '),
+					DBL_DIG,
+					this->Page.debye_lengths,
+					DBL_DIG,
+					this->Page.DDL_limit
+					);
+			}
+		}
+		else
+		{
+			ASSERT(this->Page.DT == CSurfacePage::DT_THICKNESS); // new type?
+			if (this->Page.DDL_viscosity != 1.)
+			{
+				strFormat.Format(_T("%s%4c-donnan %.*g viscosity %.*g"),
+					(LPCTSTR)s_strNewLine,
+					_T(' '),
+					DBL_DIG,
+					this->Page.thickness,
+					DBL_DIG,
+					this->Page.DDL_viscosity
+					);
+			}
+			else
+			{
+				strFormat.Format(_T("%s%4c-donnan %.*g"),
+					(LPCTSTR)s_strNewLine,
+					_T(' '),
+					DBL_DIG,
+					this->Page.thickness
+					);
+			}
+		}
+		strLines += strFormat;
+
+		if (this->Page.only_counter_ions)
+		{
+			strFormat.Format(_T("%s%4c-only_counter_ions"),
+				(LPCTSTR)s_strNewLine,
+				_T(' ')
+				);
+			strLines += strFormat;
+		}
+		break;
+
+	case BORKOVEK_DL:
+		strFormat.Format(_T("%s%4c-diffuse_layer %.*g"),
+			(LPCTSTR)s_strNewLine,
+			_T(' '),
+			DBL_DIG,
+			this->Page.thickness
+			);
+		strLines += strFormat;
+		if (this->Page.only_counter_ions)
+		{
+			strFormat.Format(_T("%s%4c-only_counter_ions"),
+				(LPCTSTR)s_strNewLine,
+				_T(' ')
+				);
+			strLines += strFormat;
+		}
+		break;
+	}
+
+	return strLines + s_strNewLine;
+}
+
+void CSurfaceSheet::Edit(CString& rStr)
+{
+	CKeywordLoader2 keywordLoader2(rStr);
+
+	struct surface* surface_ptr = &surface[0];
+
+	// Surface number
+	this->m_n_user      = surface_ptr->n_user;
+	this->m_n_user_end  = surface_ptr->n_user_end;
+	this->m_strDesc     = surface_ptr->description;
+
+	this->Page.dl_type           = surface_ptr->dl_type;
+	this->Page.type              = surface_ptr->type;
+	this->Page.thickness         = surface_ptr->thickness;
+	this->Page.sites_units       = surface_ptr->sites_units;
+	this->Page.transport         = FALSE;
+
+
+	// solution equilibration
+	if (surface_ptr->solution_equilibria)
+	{
+		this->Page.n_solution = surface_ptr->n_solution;
+		ASSERT(this->Page.n_solution > 0);
+	}
+	else
+	{
+		this->Page.n_solution = CSurfacePage::N_NONE;
+	}
+
+	// counter ions only
+	this->Page.only_counter_ions = surface_ptr->only_counter_ions;
+
+	// viscosity
+	this->Page.DDL_viscosity = surface_ptr->DDL_viscosity;
+
+	// Donnan (thickness or debye)
+	this->Page.DT = CSurfacePage::DT_THICKNESS;
+	if (surface_ptr->debye_lengths != 0.0)
+	{
+		this->Page.DT            = CSurfacePage::DT_DEBYE_LENGTHS;
+		this->Page.debye_lengths = surface_ptr->debye_lengths;
+		this->Page.DDL_limit     = surface_ptr->DDL_limit;
+	}
+
+
+
+	if (surface_ptr->count_charge > 0)
+	{
+		for (int i = 0; i < surface_ptr->count_comps; ++i)
+		{
+			const struct surface_comp* surface_comp_ptr = &(surface_ptr->comps[i]);
+			CSurfComp surfComp(surface_ptr, surface_comp_ptr);
+			if (!surfComp.m_strRate_name.IsEmpty())
+			{
+				ASSERT( surfComp.m_strPhase_name.IsEmpty() == TRUE );
+				this->Page.ListSurfComp[CSurfacePage::TI_KINETIC_REACTANTS].push_back(surfComp);
+			}
+			else if (!surfComp.m_strPhase_name.IsEmpty())
+			{
+				this->Page.ListSurfComp[CSurfacePage::TI_EQUILIBRIUM_PHASES].push_back(surfComp);
+			}
+			else
+			{
+				this->Page.ListSurfComp[CSurfacePage::TI_GENERAL].push_back(surfComp);
+			}
+
+			// check for transport
+			if (surface_ptr->transport)
+			{
+				this->Page.transport = TRUE;
+			}
+		}
+	}
+
+}
---
+++ SurfaceSheet.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "SurfacePage.h"
+
+// CSurfaceSheet
+
+#include "CommonKeywordSheet.h"
+#include "DefinedRanges.h"
+
+#ifndef baseCSurfaceSheet
+#define baseCSurfaceSheet CCommonKeywordSheet
+#endif
+
+class CSurfaceSheet : public baseCSurfaceSheet
+{
+	DECLARE_DYNAMIC(CSurfaceSheet)
+
+public:
+// COMMENT: {12/22/2009 6:18:06 PM}	CSurfaceSheet(UINT nIDCaption, CWnd* pParentWnd = NULL, UINT iSelectPage = 0);
+// COMMENT: {12/22/2009 6:18:06 PM}	CSurfaceSheet(LPCTSTR pszCaption, CWnd* pParentWnd = NULL, UINT iSelectPage = 0);
+	CSurfaceSheet(CWnd* pWndParent = NULL, CTreeCtrlNode simNode = CTreeCtrlNode());
+	virtual ~CSurfaceSheet();
+
+	virtual CString GetString();
+	virtual void Edit(CString& rStr);
+
+// COMMENT: {12/28/2009 9:22:47 PM}	enum EquilSolutionNum
+// COMMENT: {12/28/2009 9:22:47 PM}	{
+// COMMENT: {12/28/2009 9:22:47 PM}		N_NONE = -1
+// COMMENT: {12/28/2009 9:22:47 PM}	};
+	std::set<CDBRange>  SetSolutions;
+	CString             SolutionNumberFormat;
+// COMMENT: {12/28/2009 9:22:50 PM}	int                 EquilSolutionNum;
+
+	CDefinedRanges  DefinedRanges;
+
+protected:
+	DECLARE_MESSAGE_MAP()
+
+	CSurfacePage    Page;
+
+};
+
+
