241fc69cd15faccaab77197847ec80c6f1538aff
Scott R Charlton <charlton@usgs.gov>
2010-05-26 04:02:21 +0000

    fixed missing parens or quotes bug in CBasicObj
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqci/trunk@4468 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- resource.h
+++ resource.h
@@ -760,6 +760,9 @@
 #define IDS_STRING701                   701
 #define IDS_STRING702                   702
 #define IDS_STRING703                   703
+#define IDS_ERR_MISSING_Q               704
+#define IDS_ERR_MISSING_LP              705
+#define IDS_ERR_MISSING_RP              706
 #define IDC_E_OUTPUT                    1000
 #define IDC_BULB                        1000
 #define IDC_MSHFLEXGRID1                1000
@@ -1653,7 +1656,7 @@
 #define IDS_OLD_IDLEMESSAGE             57347
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
