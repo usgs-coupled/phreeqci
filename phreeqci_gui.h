dc6353a18e18299d093941cb3188c0b82659ab42
Scott R Charlton <charlton@usgs.gov>
2012-04-11 04:33:28 +0000

    cleanup
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqci/branches/phreeqc3@6453 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- phreeqci_gui.h
+++ phreeqci_gui.h
@@ -7,40 +7,6 @@
 #define AFX_PHREEQCI_GUI_H__A62CB467_D42F_4F92_A796_A026F88518B3__INCLUDED_
 
 #include <windows.h>
-#include <crtdbg.h>
-
-struct h_status {
-	HWND hText;
-	HWND hSim;
-	HWND hTrans;
-};
-
-extern struct h_status g_status;
-extern char g_szLineBuf[160];
-extern HANDLE g_hKill;
-extern BOOL g_bKill;
-extern int linenr;
-
-// returns	TRUE if the line was copied
-//			FALSE if a larger buffer is required to copy the entire line
-//				pcb returns the buffer size required to copy the entire line
-typedef BOOL (CALLBACK *LINECALLBACK)(DWORD dwCookie, LPBYTE pbBuff, LONG cb, LONG *pcb);
-
-typedef void (CALLBACK *ERRORCALLBACK)(DWORD dwCookie, LPCSTR lpcstr);
-
-typedef void (CALLBACK *WARNCALLBACK)(DWORD dwCookie, LPCSTR lpcstr);
-
-typedef struct tagGUICALLBACK
-{
-	DWORD dwCookie;		/* user value passed to callback as first parameter */
-	LINECALLBACK pfnLine;
-	ERRORCALLBACK pfnError;
-	WARNCALLBACK pfnWarn;
-} GUICALLBACK;
-
-typedef void (*LPFNPHREEQC)(GUICALLBACK gui);
-typedef void (*LPFNPHREEQC2)(GUICALLBACK gui, int nValue);
-
 
 // see winerror.h
 #define MAKESOFTWAREEXCEPTION(Severity, Facility, Exception) \
